79b9d699f0f06feee2b39549d37ea1922468b944
Samantha Ashby <samanthaashby@Samanthas-MacBook-Pro.local>
2020-09-15 08:00:10 -0400

    Added some plots on 9/15

--- Proj1_Working.R
+++ Proj1_Working.R
@@ -77,22 +77,43 @@ get_stats(ID=54)
 get_hockey_data<-function(endpoint=NULL,ID=NULL,mod=NULL){
   if(is.null(endpoint)){stop("Need valid endpoint")}
     else if (endpoint=="franchise_data"){get_franchise_data(ID)}
+    else if (endpoint=="franchise_totals"){get_franchise_totals(ID)}
     else if (endpoint=="franchise_records"){get_franchise_records(ID)}
     else if (endpoint=="goalie_records"){get_goalie_records(ID)}
     else if (endpoint=="skater_records"){get_skater_records(ID)} 
     else if (endpoint=="team_stats"){get_stats(ID)} 
 }
 
-get_hockey_data(endpoint="franchise_data")
 
-#do a join
-x<-get_hockey_data(endpoint="goalie_records",ID=5)
-x
-y<-get_hockey_data(endpoint="franchise_records",ID=5)
-y
+#Create a a new column that indicates whether team existed before World War II
+a<-get_hockey_data(endpoint="team_stats")
+a<-mutate(a,preWWII=(as.numeric(teams.firstYearOfPlay)<1939))
 
-x$teams.name
-select(x,teams.franchise.teamName,teams.franchise.franchiseId)
+#Create a contingency table to show which conference (Eastern or Western) has
+#more pre-WWII teams
+table(a$preWWII,a$teams.conference.name)
 
+#Set options so that R will print all rows of tibble
+options(tibble.print_max = Inf)
 
+#Get goalie data for Carolina Hurricanes franchise
+b<-get_hockey_data(endpoint="goalie_records",ID=26)
 
+#Create a column that indicates whether a goalie is a longTimer, meaning they 
+#have played more games than the average goalie (within the Canes franchise)
+b<-mutate(b,longTimer=(data.gamesPlayed>mean(b$data.gamesPlayed)))
+
+#Create a table telling us if any of the active goalies are longTimers
+table(b$data.activePlayer,b$longTimer)
+
+#Get total games by franchise
+c<-get_hockey_data(endpoint="franchise_totals")
+c<-c%>%group_by(data.franchiseId)%>%summarise(totalGames=sum(data.gamesPlayed))
+
+#Get franchise data
+d<-get_hockey_data(endpoint="franchise_data")
+d<-rename(d,data.franchiseId=data.id)
+e<-inner_join(c,d)
+attributes(e)
+e<-mutate(e,active=is.na(data.lastSeasonId))
+table(e$active)
